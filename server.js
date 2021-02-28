818cc8c1221ce6c28643e6edf2435cd976d895c1
Shannon Reed <44705472+shanreed@users.noreply.github.com>
2021-02-27 16:35:29 -0800

    changed friends to tinkers

--- server.js
+++ server.js
@@ -14,48 +14,48 @@ const sendUserError = (msg, res) => {
   };
 
 //Dummy data
-  let friends = [
+  let tinkers = [
     {
       id: 1,
       name: 'Marie Archange',
       age: 43,
-      email: 'marie@friends.com'
+      email: 'marie@tinkers.com'
     },
     {
       id: 2,
       name: 'John Smith',
       age: 28,
-      email: 'john@friends.com'
+      email: 'john@tinkers.com'
     },
     {
       id: 3,
       name: 'Mike Jones',
       age: 45,
-      email: 'mike@friends.com'
+      email: 'mike@tinkers.com'
     },
     {
       id: 4,
       name: 'David Williams',
       age: 35,
-      email: 'david@friends.com'
+      email: 'david@tinkers.com'
     },
     {
       id: 5,
       name: 'Juan Lewis',
       age: 30,
-      email: 'juan@friends.com'
+      email: 'juan@tinkers.com'
     },
     {
       id: 6,
       name: 'Paul Daniel',
       age: 39,
-      email: 'paul@friends.com'
+      email: 'paul@tinkers.com'
     }
   ];
   
 
- //for adding new friends
-  let friendId = 7;
+ //for adding new tinkers
+  let tinkerId = 7;
 
 
 //Gets the server message
@@ -63,110 +63,110 @@ const sendUserError = (msg, res) => {
     res.json({message: "Welcome to the server"});
   });
 
-//Gets the entire friend list
-  server.get("/friends", (req, res) => {
-    res.json(friends);
+//Gets the entire tinker list
+  server.get("/tinkers", (req, res) => {
+    res.json(tinkers);
   });
 
 
-//Gets Friend by ID
-  server.get("/friendById/:id", (req, res) => {
+//Gets Tinker by ID
+  server.get("/tinkerById/:id", (req, res) => {
       //Pulls the id off the url
     const { id } = req.params;
 
-    //finds the friend with that id
-    const findFriendById = friend => {
-      return friend.id == id;
+    //finds the tinker with that id
+    const findTinkerById = tinker => {
+      return tinker.id == id;
     };
-    const foundFriend = friends.find(findFriendById);
+    const foundTinker = tinkers.find(findTinkerById);
 
-    //if no friend with that id exists
-    if (!foundFriend) {
-      return sendUserError("No Friend found by that ID", res);
+    //if no tinker with that id exists
+    if (!foundTinker) {
+      return sendUserError("No Tinker found by that ID", res);
     } else {
-      res.json(foundFriend);
+      res.json(foundTinker);
     }
   });
 
-//Adds a friend to the list
-  server.post("/friends", (req, res) => {
+//Adds a tinker to the list
+  server.post("/tinkers", (req, res) => {
       //Pulls name, age and email off the request body
     const { name, age, email } = req.body;
 
-    //adds the id(which is whatever is stored in the friendId variable), name, age and email to the newFriend variable
-    const newFriend = {  id: friendId, name, age, email};
+    //adds the id(which is whatever is stored in the tinkerId variable), name, age and email to the newTinker variable
+    const newTinker = {  id: tinkerId, name, age, email};
 
     //if no name, age or email is added
     if (!name || !age || !email) {
       return sendUserError(
-        "You forgot something! Name, Age, and Email are all required to create an friend in the friend DB.",
+        "You forgot something! Name, Age, and Email are all required to create an tinker in the tinker DB.",
         res
       );
     }
-    //checks to see if the friend with that name already exixts
-    const findFriendByName = friend => {
-      return friend.name === name;
+    //checks to see if the tinker with that name already exixts
+    const findTinkerByName = tinker => {
+      return tinker.name === name;
     };
-    if (friends.find(findFriendByName)) {
+    if (tinkers.find(findTinkerByName)) {
       return sendUserError(
-        `NO WAY!!!! ${name} already exists in the friend DB.`,
+        `NO WAY!!!! ${name} already exists in the tinker DB.`,
         res
       );
     }
   
-//add the newFriend to the friends list
-    friends.push(newFriend);
-    friendId++;//add one to the friendId variable, so the next friend added does not have the same id
-    res.json(friends);
+//add the newTinker to the tinkers list
+    tinkers.push(newTinker);
+    tinkerId++;//add one to the tinkerId variable, so the next tinker added does not have the same id
+    res.json(tinkers);
   });
 
 
-//Update friend
-server.put("/friends/:id", (req, res) => {
+//Update tinker
+server.put("/tinkers/:id", (req, res) => {
     //pulls the id off of the url
     const { id } = req.params;
 
     //pulls the name, age and email off of the body of the request
     const { name, age, email } = req.body;
     
-    //finds the friend with the specified id and returns that id
-    const findFriendById = friend => {
-      return friend.id == id;
+    //finds the tinker with the specified id and returns that id
+    const findTinkerById = tinker => {
+      return tinker.id == id;
     };
 
 //
-    const foundFriend = friends.find(findFriendById);
+    const foundTinker = tinkers.find(findTinkerById);
 
-    //if foundFriend is null
-    if (!foundFriend) {
-      return sendUserError("No Friend found with that ID", res);
+    //if foundTinker is null
+    if (!foundTinker) {
+      return sendUserError("No Tinker found with that ID", res);
 
 
     } else {
-        //if friend found
-      if (name) foundFriend.name = name;//update name
-      if (age) foundFriend.age = age;//update age
-      if (email) foundFriend.email = email;//update email
-      res.json(friends);
+        //if tinker found
+      if (name) foundTinker.name = name;//update name
+      if (age) foundTinker.age = age;//update age
+      if (email) foundTinker.email = email;//update email
+      res.json(tinkers);
     }
   });
 
 
-//Delete Friend
-server.delete("/friends/:id", (req, res) => {
+//Delete Tinker
+server.delete("/tinkers/:id", (req, res) => {
     //pulls the id off of the url
     const { id } = req.params;
 
-    //finds the friend with the id from the pramas, and stores it in the foundFriend variable
-    const foundFriend = friends.find(friend => friend.id == id);
+    //finds the tinker with the id from the pramas, and stores it in the foundTinker variable
+    const foundTinker = tinkers.find(tinker => tinker.id == id);
   
-    if (foundFriend) { //if foundFriend is not null
+    if (foundTinker) { //if foundTinker is not null
 
-        //setting friends to all the firends in the list except the friend with the specified id
-      friends = friends.filter(friend => friend.id != id);
-      res.status(200).json(friends);
+        //setting tinkers to all the firends in the list except the tinker with the specified id
+      tinkers = tinkers.filter(tinker => tinker.id != id);
+      res.status(200).json(tinkers);
     } else {
-      sendUserError("No friend by that ID exists in the friend DB", res);
+      sendUserError("No tinker by that ID exists in the tinker DB", res);
     }
   });
 
